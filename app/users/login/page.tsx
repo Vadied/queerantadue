300927f6a68676bfcb75fdcca7a6e86232a0547f
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-12-03 13:00:15 +0100

    remove registration

--- app/users/login/page.tsx
+++ app/users/login/page.tsx
@@ -8,12 +8,12 @@ const Page = async () => {
       <h1 className="text-xl text-center">Fai qui il tuo login</h1>
       <div className="bg-background-lighter rounded p-10">
         <LoginForm />
-        <div className="flex flex-col items-end text-sm">
+        {/* <div className="flex flex-col items-end text-sm">
           <p>Non sei registrato?</p>
           <Link href={register.href} className="text-link hover:text-white">
             Registrati qui
           </Link>
-        </div>
+        </div> */}
       </div>
     </>
   );
