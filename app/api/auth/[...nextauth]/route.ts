414948a8e98385f278754c7331e46a712c7c2fd9
Davide Filippi <9254428-Vadied@users.noreply.gitlab.com>
2023-12-03 13:11:24 +0100

    try fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,8 @@ import NextAuth from 'next-auth';
 
 import authOptions from './authOptions';
 
+export const maxDuration = 30; // This function can run for a maximum of 5 seconds
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
